74fb83a5a07ca4ecf6e3f59103efb6b939089b96
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-05-30 14:59:31 +0100

    adding tests for turnNumber

--- scripts/simonGame.js
+++ scripts/simonGame.js
@@ -2,6 +2,7 @@ let game = {
   score: 0,
   currentGame: [],
   playerMoves: [],
+  turnNumber: 0,
   choices: ["button1", "button2", "button3", "button4"],
 };
 function newGame() {
@@ -14,7 +15,7 @@ function newGame() {
 function addTurn() {
   game.playerMoves = [];
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
-  //showTurns();
+  showTurns();
 }
 function lightsOn(circ) {
   document.getElementById(circ).classList.add("light");
@@ -25,5 +26,14 @@ function lightsOn(circ) {
 function showScore() {
   document.getElementById("score").innerText = game.score;
 }
-
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+  game.turnNumber = 0;
+  let turns = setInterval(() => {
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
+  }, 800);
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns,};
