202b99bea304505afad2756ef3078eb998dd5003
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-05-30 14:27:33 +0100

    Adding tests and function for those tests to file

--- scripts/simonGame.js
+++ scripts/simonGame.js
@@ -1,4 +1,29 @@
 let game = {
-    score: 0,
+  score: 0,
+  currentGame: [],
+  playerMoves: [],
+  choices: ["button1", "button2", "button3", "button4"],
 };
-module.exports = {game};
+function newGame() {
+  game.score = 0;
+  game.currentGame = [];
+  game.playerMoves = [];
+  showScore();
+  addTurn();
+}
+function addTurn() {
+  game.playerMoves = [];
+  game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
+  //showTurns();
+}
+function lightsOn(circ) {
+  document.getElementById(circ).classList.add("light");
+  setTimeout(() => {
+    document.getElementById(circ).classList.remove("light");
+  }, 400);
+}
+function showScore() {
+  document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
